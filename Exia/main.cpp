f602ef6bf689903e7709359d450bbf52b192c85f
JaZziW <grd.fabien@gmail.com>
2015-03-24 10:29:32 +0100

    Testage

---
+++ Exia/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Fabien
+ *
+ * Created on 24 mars 2015, 10:28
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
